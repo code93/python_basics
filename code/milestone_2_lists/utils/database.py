646beec2df163ec9859ad0cf526b4afacae7a67e
shubham birmi <shubhambirmi@hotmail.com>
2024-06-26 04:28:17 +0530

    database sqlite python

--- code/milestone_2_lists/utils/database.py
+++ code/milestone_2_lists/utils/database.py
@@ -1,49 +1,99 @@
+from .database_connection import DatabaseConnection
+
 """
-Concerned with storing and retrieving books from a json file.
-[
-    {
-        'name': 'Clean Code',
-        'author': 'Robert',
-        'read': True
-    }
-]
+Concerned with storing and retrieving books from a database.
 """
-import json
-books_file = 'books.json'
 
 def create_book_table():
-    with open(books_file, 'w') as file:
-        json.dump([], file)
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+
+        cursor.execute('CREATE TABLE IF NOT EXISTS books(name text primary key, author text, read integer)')
 
 def add_book(name,author):
-    books = get_all_books()
-    books.append({'name': name, 'author': author, 'read': False})
-    _save_all_books(books)
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+
+        cursor.execute('INSERT INTO books VALUES(?,?,0)',(name, author))
+
 
 def get_all_books():
-    with open(books_file, 'r') as file:
-        return json.load(file)
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
 
-def _save_all_books(books):
-    with open(books_file, 'w') as file:
-        json.dump(books, file)
+        cursor.execute('SELECT * FROM books')
+        books = [{'name': row[0], 'author': row[1], 'read':row[2]} for row in cursor.fetchall()]
+    return books
+
+# def _save_all_books(books):
+#     with open(books_file, 'w') as file:
+#         json.dump(books, file)
 
 def view_book(name):
     books = get_all_books()
     return [book for book in books if book['name']==name]
 
 def mark_read(name):
-    books = get_all_books()
-    for book in books:
-        if book['name'] == name:
-            book['read'] = True
-    _save_all_books(books)
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+
+        cursor.execute('UPDATE books SET read=1 WHERE name=?',(name,))
+
 
 
 def delete_book(name):
-    books = get_all_books()
-    books = [book for book in books if book['name'] != name]
-    _save_all_books(books)
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+
+        cursor.execute('DELETE FROM books WHERE name=?',(name,))
+
+
+# """
+# Concerned with storing and retrieving books from a json file.
+# [
+#     {
+#         'name': 'Clean Code',
+#         'author': 'Robert',
+#         'read': True
+#     }
+# ]
+# """
+# import json
+# books_file = 'books.json'
+
+# def create_book_table():
+#     with open(books_file, 'w') as file:
+#         json.dump([], file)
+
+# def add_book(name,author):
+#     books = get_all_books()
+#     books.append({'name': name, 'author': author, 'read': False})
+#     _save_all_books(books)
+
+# def get_all_books():
+#     with open(books_file, 'r') as file:
+#         return json.load(file)
+
+# def _save_all_books(books):
+#     with open(books_file, 'w') as file:
+#         json.dump(books, file)
+
+# def view_book(name):
+#     books = get_all_books()
+#     return [book for book in books if book['name']==name]
+
+# def mark_read(name):
+#     books = get_all_books()
+#     for book in books:
+#         if book['name'] == name:
+#             book['read'] = True
+#     _save_all_books(books)
+
+
+# def delete_book(name):
+#     books = get_all_books()
+#     books = [book for book in books if book['name'] != name]
+#     _save_all_books(books)
             
 
 
